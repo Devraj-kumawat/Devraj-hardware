601214da79800826aeffed2ed573b79e4ba9fa45
Devraj-kumawat <2023pietaddevraj019@poornima.org>
2025-11-01 19:09:12 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend', // 👈 tell vite where index.html lives
+  build: {
+    outDir: '../dist', // output folder
+  },
   plugins: [react()],
 })
-
